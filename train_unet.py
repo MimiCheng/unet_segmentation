7c0bdd8de78301762edf5801f88026d5e108950e
mimicheng <mimi@vm-training-gpu-1.c.astute-engine-213307.internal>
2018-11-25 10:00:42 +0000

    rearrange folder

--- train_unet.py
+++ train_unet.py
@@ -1,6 +1,6 @@
 import numpy as np
 from keras.models import Model
-from keras.layers import Input, Conv2D, MaxPooling2D, UpSampling2D, concatenate, Cropping2D, ZeroPadding2D
+from keras.layers import Input, Conv2D, MaxPooling2D, UpSampling2D, concatenate, Cropping2D, ZeroPadding2D, Conv2DTranspose
 from keras.optimizers import Adam
 from keras.callbacks import ModelCheckpoint, LearningRateScheduler, TensorBoard
 from keras import backend as K
@@ -8,13 +8,13 @@ import os
 from skimage.transform import resize
 from skimage.io import imsave
 
-
+K.set_image_dim_ordering('th')  # Theano dimension ordering in this code
 main_path = "/disk1/luna16/"
 working_path = "/disk1/luna16/output/"
 
-K.set_image_dim_ordering('th')  # Theano dimension ordering in this code
-BATCH_SIZE=16
-EPOCHS=80
+
+BATCH_SIZE=8
+EPOCHS=60
 img_rows = 512
 img_cols = 512
 
@@ -40,6 +40,7 @@ def dice_coef_loss(y_true, y_pred):
 
 
 def get_model():
+
     inputs = Input((1, img_rows, img_cols))
 
     conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(inputs)
@@ -85,6 +86,54 @@ def get_model():
     return model
 
 
+def get_unet():
+    
+    K.set_image_dim_ordering('th')  # Theano dimension ordering in this code
+    inputs = Input((1, img_rows, img_cols))
+
+    conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(inputs)
+    conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv1)
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+
+    conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(pool1)
+    conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv2)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+
+    conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(pool2)
+    conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv3)
+    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+
+    conv4 = Conv2D(256, (3, 3), activation='relu', padding='same')(pool3)
+    conv4 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv4)
+    pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
+
+    conv5 = Conv2D(512, (3, 3), activation='relu', padding='same')(pool4)
+    conv5 = Conv2D(512, (3, 3), activation='relu', padding='same')(conv5)
+
+    up6 = concatenate([Conv2DTranspose(256, (2, 2), strides=(2, 2), padding='same')(conv5), conv4], axis=1)
+    conv6 = Conv2D(256, (3, 3), activation='relu', padding='same')(up6)
+    conv6 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv6)
+
+    up7 = concatenate([Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same')(conv6), conv3], axis=1)
+    conv7 = Conv2D(128, (3, 3), activation='relu', padding='same')(up7)
+    conv7 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv7)
+
+    up8 = concatenate([Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same')(conv7), conv2], axis=1)
+    conv8 = Conv2D(64, (3, 3), activation='relu', padding='same')(up8)
+    conv8 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv8)
+
+    up9 = concatenate([Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same')(conv8), conv1], axis=1)
+    conv9 = Conv2D(32, (3, 3), activation='relu', padding='same')(up9)
+    conv9 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv9)
+
+    conv10 = Conv2D(1, (1, 1), activation='sigmoid')(conv9)
+
+    model = Model(inputs=[inputs], outputs=[conv10])
+    print (model.summary())
+    model.compile(optimizer=Adam(lr=1e-5), loss=dice_coef_loss, metrics=[dice_coef])
+
+    return model
+
 def train_and_predict(use_existing):
     print('-' * 30)
     print('Loading and preprocessing train data...')
@@ -110,9 +159,9 @@ def train_and_predict(use_existing):
 
     model = get_model()
     # Saving weights to unet.hdf5 at checkpoints
-    best_weight_path = '../model/best_unet_{}.hdf5'.format(BATCH_SIZE)
+    best_weight_path = '../model/best_unet_upsampling_{}.hdf5'.format(BATCH_SIZE)
     model_checkpoint = ModelCheckpoint(best_weight_path, monitor='val_loss', save_best_only=True)
-    tb = TensorBoard(log_dir="../logs", batch_size=BATCH_SIZE)
+    tb = TensorBoard(log_dir="../logs_upsampling", batch_size=BATCH_SIZE)
     # Set argument for call to train_and_predict to true at end of script
     if use_existing:
         print('loading weights...')
